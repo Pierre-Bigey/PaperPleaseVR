c7698457d30a65d990b11774a0b7796da3b68fb2
Pierre Bigey <pierre.bigey1@gmail.com>
2023-11-09 21:35:34 +0100

    Stamping corrected
    I had to initialize the stamp in a parent with scale 1:1 and rotate it so that it matches the orientation of the stamper. Finally done

--- Assets/Scripts/Stamper.cs
+++ Assets/Scripts/Stamper.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -25,14 +26,51 @@ public class Stamper : MonoBehaviour
         GameObject stamp = Instantiate(AccessStamp);
         stamp.transform.position = hit.point;
         stamp.transform.position += hit.normal * 0.001f;
+        //stamp.transform.rotation = quaternion.identity;
+        //stamp.transform.SetParent(passport.transform);
+        Vector3 stampUp = Vector3.ProjectOnPlane(this.transform.forward, passport.transform.up);
+        //stamp.transform.rotation.SetLookRotation(-passport.transform.up,stampUp);
         
+        stamp.transform.SetParent(passport.transform.parent,true);
+
+        stamp.transform.localEulerAngles = new Vector3(90, 0, 0);
+        float angle = Vector3.SignedAngle(stampUp,stamp.transform.up,transform.up );
+        stamp.transform.localEulerAngles = new Vector3(90, 0, angle);
         /*stamp.transform.right = transform.right;
         stamp.transform.up = transform.forward;*/
-        stamp.transform.rotation = Quaternion.Euler(this.transform.eulerAngles + new Vector3(90, 0, 0));
-        stamp.transform.forward = - hit.normal;
-        float angle = Vector3.SignedAngle(stamp.transform.right, transform.right,stamp.transform.forward);
-        stamp.transform.Rotate(stamp.transform.right,angle);
-        //stamp.transform.SetParent(passport.transform, true);
+        //stamp.transform.rotation = Quaternion.Euler(this.transform.eulerAngles + new Vector3(90, 0, 0));
+        //stamp.transform.forward = - hit.normal;
+        //float angle = Vector3.SignedAngle(stamp.transform.right, transform.right,stamp.transform.forward);
+        //stamp.transform.Rotate(stamp.transform.right,angle);
+
+
+        //New approach
+        /*var stamp = Instantiate(AccessStamp,passport.transform);
+
+        stamp.transform.localRotation = Quaternion.identity;
+
+        Vector3 passportLocalScale = passport.transform.localScale;
+
+        stamp.transform.localPosition = passport.transform.InverseTransformPoint(hit.point);
+        stamp.transform.localPosition += passport.transform.up*passportLocalScale.y*0.01f;
+        /*stamp.transform.localPosition = new Vector3(
+                                           corner.x / passport.transform.localScale.x,
+                                           corner.y / passport.transform.localScale.y,
+                                           corner.z / passport.transform.localScale.z
+                                       )
+
+                                       + new Vector3(
+                                           stepX / passport.transform.localScale.x,
+                                           center.y + 0.15f,
+                                           (stepZ * 1.5f + i * stepZ) / passport.transform.localScale.z
+                                       );#1#
+
+        stamp.transform.localScale = new Vector3(
+            stamp.transform.localScale.x / passportLocalScale.x,
+            stamp.transform.localScale.y / passportLocalScale.y,
+            stamp.transform.localScale.z / passportLocalScale.z
+        );*/
+
     }
 
     private void OnCollisionEnter(Collision collision)
